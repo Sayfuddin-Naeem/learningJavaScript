716b634df4f48912456065023a0a4f70f13b568d
Sayfuddin-Naeem <sayfuddinnaeem.cse4.bu@gmail.com>
2025-04-23 17:54:02 +0600

    Learning Basics done

---
+++ Basic/intro.js
@@ -0,0 +1,20 @@
+console.log("Welcome to Learning js course.");
+
+// let userAge = 25;
+// let isCitizen = true;
+// let isRegistered = false;
+
+// if(userAge >= 18){
+//     if(isCitizen){
+//         if(isRegistered){
+//             console.log("You are eligible to vote.");
+//         }else{
+//             console.log("You are not eligible due to registration status");
+//         }
+//     }else{
+//         console.log("You are not eligible due to citizenship status.");
+//     }
+// }else{
+//     console.log("You are not eligible to vote.")
+// }
+
